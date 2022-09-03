b2e08a5b312e1fa779500d4c384e1832b324149c
eunsun <nammugmuhg@gmail.com>
2022-09-03 15:36:58 +0900

    ch12 make fake data

---
+++ ch12/src/fake/makeFakeData.ts
@@ -0,0 +1,13 @@
+import Chance from "chance"; //chance 패키지
+import { IFake } from "./IFake";
+
+const c = new Chance();
+export const makeFakeData = (): IFake => ({
+  name: c.name(),
+  email: c.email(),
+  profession: c.profession(),
+  birthday: c.birthday(),
+  sentence: c.sentence(),
+});
+
+export {IFake}
---
+++ ch12/src/test/makeFakeData-test.ts
@@ -0,0 +1,5 @@
+import { makeFakeData, IFake } from "../fake/makeFakeData";
+
+const fakeData: IFake = makeFakeData();
+
+console.log(fakeData);
