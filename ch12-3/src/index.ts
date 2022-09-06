6442b3f332b0dc540cc06a3850e48759b093df17
Grapefruitgreentealoe <nammugmuhg@gmail.com>
2022-09-07 04:43:09 +0900

    ch12-3 express

---
+++ ch12-3/src/index.ts
@@ -0,0 +1,12 @@
+import { connect } from "../../ch12-2/src/mongodb/connect";
+import { runServer } from "./runServer";
+
+connect()
+  .then(async (connection) => {
+    const db = await connection.db("ch12-2");
+    return db;
+  })
+  .then(runServer)
+  .catch((e: Error) => {
+    console.log(e.message);
+  });
